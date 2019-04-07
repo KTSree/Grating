fda24d1f141fbdf9c8a5836c8486387bf191d10a
ParvN <parvathyn8@gmail.com>
2019-04-07 11:08:55 +0200

    Added description about grating function

---
+++ sinusoidal_grating.py
@@ -0,0 +1,18 @@
+'''
+
+Simple function to generate sinusoidal grating visual stimulus
+
+parameters:
+
+x,y      :  Two dimensional grid of photoreceptors
+theta    :  Orientation
+K        :  Spatial frequency
+phi      :  Spatial phase
+A        :  Contrast amplitude
+
+returns:
+
+sinusoidal grating corresponding to the given parameters ( mentioned above)
+
+'''
+
