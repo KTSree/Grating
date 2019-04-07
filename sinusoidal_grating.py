f4575e90ee5c01eb07675abc6ffb876efe536456
ParvN <parvathyn8@gmail.com>
2019-04-07 20:56:39 +0200

    Added grid generation function

--- sinusoidal_grating.py
+++ sinusoidal_grating.py
@@ -16,3 +16,24 @@ sinusoidal grating corresponding to the given parameters ( mentioned above)
 
 '''
 
+import numpy as np
+import matplotlib.pyplot as plt
+import math as m
+
+x0 = 5  # given x0=5,y0=5
+y0 = 5
+
+delta_x = 1/10  # Sampling
+delta_y = 1/10
+
+max_x = (2*x0)/delta_x  # as per visual field description given
+max_y = (2*y0)/delta_y
+
+def grid_generation():
+    # Function to generate the 2-D grid [x,y]
+    x_range=np.arange(-x0,x0,delta_x)
+    y_range=np.arange(-y0,x0,delta_y)
+    x,y=np.meshgrid(x_range,y_range)
+    return x,y
+
+
